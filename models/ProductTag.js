088f203f374eb78576bc1dc20ecde991089a26d6
Jasper Oudemans <jasperoudemans@gmail.com>
2022-02-04 15:43:57 -0700

    got associations

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,38 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
+const Product = require("./Product");
+const Tag = require("./Tag");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: { model: Product, key: "id" },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: { model: Tag, key: "id" },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
+
+Product.belongsToMany(Tag, { through: "ProductTag" });
+Tag.belongsToMany(Product, { through: "ProductTag" });
 
 module.exports = ProductTag;
